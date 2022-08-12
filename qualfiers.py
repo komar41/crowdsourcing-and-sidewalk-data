964922f0b04729326afed2089a57b8601ce4a467
komar3 <komar3@uic.edu>
2022-08-11 20:35:34 -0400

    osm quality

--- qualfiers.py
+++ qualfiers.py
@@ -1,3 +1,5 @@
+is_road = "(type == 'W') and ('highway' in tags)"
+
 def poi_qualifier(tags, type):
     return ('amenity' in tags or 'shop' in tags or 'tourism' in tags)
 
@@ -5,13 +7,56 @@ def building_qualifier(tags, type):
     return (type == 'W' or type == 'R') and ( ('building' in tags) or ('building:part' in tags) or (tags.get('type') == 'building') ) and ( (tags.get('location') != 'underground') or ('bridge' not in tags) )
 
 def road_qualifier(tags, type):
-    return (type == 'W') and ('highway' in tags)
+    return eval(is_road)
 
-def sidewalk_qualifier(tags, type):
-    return (type == 'W' and 'highway' in tags) and ('sidewalk' in tags or 'sidewalk' in tags.values() or \
-            'footpath' in tags or 'footpath' in tags.values() or \
-            'footway' in tags or 'footway' in tags.values())
+def roads_with_sidewalk(tags, type):
+    return eval(is_road) and \
+        ( 
+            ('sidewalk' in tags and tags.get('sidewalk') in ['yes', 'left', 'right', 'both', 'separate']) or \
+            ('sidewalk:left' in tags and tags.get('sidewalk:left') in ['yes', 'separate']) or \
+            ('sidewalk:right' in tags and tags.get('sidewalk:right') in ['yes', 'separate']) or \
+            ('foot' in tags and tags.get('foot') == 'use_sidepath')
+        )
 
+def roads_without_sidewalk(tags, type):
 
-def sidewalk_qualifer_x(tags, type):
-    return (type == 'W' and 'highway' in tags) and ('footway' in tags or 'sidewalk' in tags.values())
+    case1, case2, case3 = True, True, True
+
+    if 'sidewalk' in tags: 
+        case1 = tags.get('sidewalk') in ['no', 'none']
+    
+    if 'sidewalk:left' in tags:
+        case2 = tags.get('sidewalk:left') in ['no', 'none']
+
+    if 'sidewalk:right' in tags:
+        case3 = tags.get('sidewalk:right') in ['no', 'none']
+
+    return eval(is_road) and (case1 and case2  and case3)
+
+def roads_without_sidewalk_tag(tags, type):
+    return eval(is_road) and \
+        (
+            tags.get('highway') not in ['footway', 'escape', 'raceway', 'busway', 'bridleway', 'steps', 'corridor', 'path', 'cycleway', 'construction'] and
+            not roads_with_sidewalk(tags, type)
+        )
+
+def footway_qualifier(tags, type):\
+
+    return eval(is_road) and \
+    (
+            (tags.get('highway') in ['footway', 'living_street', 'pedestrian']) or
+            ('foot' in tags and tags.get('foot') in ['yes', 'designated', 'permissive', 'private', 'destination'])
+    )
+    '''
+        foot:private -> Its only for the owners to walk or people they give permission to
+        foot:destination -> The public has right of access only if this is the only road to your destination. This route should only be used as a means of getting to or from a specific point. It should not be used for transit to somewhere else.
+    '''
+
+def sidewalk_qualifer(tags, type):
+    return eval(is_road) and (tags.get('highway') == 'footway' and tags.get('footway')=='sidewalk')
+
+def road_crossing_qualifer(tags, type):
+    return eval(is_road) and (tags.get('highway') == 'footway' and tags.get('footway')=='crossing')
+
+def bridge_qualifier(tags, type):
+    return eval(is_road) and (tags.get('highway') == 'footway' and 'bridge' in tags)
